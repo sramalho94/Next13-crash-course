556d36752847059beea3765908bc0022b38e6db9
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-29 09:32:09 -0500

    incorporated api call and movie component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
-export default function Home() {
+import Movie from './about/Movie'
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+  console.log(res)
   return (
     <main>
       <h1 className="text-lg py-2 m-4">Hello Next 13</h1>
+      <div className="grid gap-16 grid-cols-fluid">
+        {res.results.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        ))}
+      </div>
     </main>
   )
 }
