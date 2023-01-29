02ffd9a7c417ae5d093502b844bbb514ecbceba5
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-29 09:34:12 -0500

    added new page for movie detail with tailwind utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
     extend: {
       fontFamily: {
         montserrat: ['var(--font-montserrat)']
+      },
+      gridTemplateColumns: {
+        fluid: 'repeat(auto-fit,minmax(15rem,1fr))'
       }
     }
   },
